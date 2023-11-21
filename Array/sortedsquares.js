e204a24680ad04bbd17f6d877582488d2ca66b60
Zach Hamilton <zachmhamilton@outlook.com>
2023-11-20 22:11:07 -0500

    sortedsquares completed

---
+++ Array/sortedsquares.js
@@ -0,0 +1,16 @@
+var sortedSquares = function(nums) {
+  const result = [];
+  let left = 0;
+  let right = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+      if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+          result[i] = nums[right] ** 2;
+          right--;
+      } else {
+          result[i] = nums[left] ** 2;
+          left++;
+      }
+  }
+  return result;
+};
